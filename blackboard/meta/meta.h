b91f34ff9211857011d2ceebc13e317267dec2a6
Luca <luca@dashandslash.com>
2022-03-03 10:54:10 +0100

    Cleanup meta component registration

--- blackboard/meta/meta.h
+++ blackboard/meta/meta.h
@@ -16,18 +16,17 @@ struct Info
     const entt::type_info type_info;
 };
 
-using Meta_infos = std::unordered_map<entt::id_type, Info>;
+using Meta_infos = entt::dense_map<entt::id_type, Info>;
+
+inline constexpr auto property_name_key = entt::hashed_string("name");
 
 namespace impl {
-inline constexpr auto name_hs = entt::hashed_string("name");
+
 inline Meta_infos meta_components_infos;
-}    // namespace impl
 
 template<typename T>
-inline void register_component(std::string &&reflected_name)
+inline void fn(std::string &&reflected_name)
 {
-    using namespace entt::literals;
-    using namespace impl;
     const entt::type_info type_info = entt::type_id<T>();
 
     if (meta_components_infos.contains(type_info.hash()))
@@ -35,15 +34,27 @@ inline void register_component(std::string &&reflected_name)
         return;
     }
 
-    entt::meta<T>().type(entt::hashed_string(reflected_name.c_str())).prop(name_hs, reflected_name);
-    const entt::meta_type meta_type = entt::resolve<T>();
+    entt::meta<T>()
+      .type(entt::hashed_string(reflected_name.c_str()))
+      .prop(property_name_key, reflected_name);
 
-    if (auto prop = meta_type.prop(name_hs); prop)
+    // To ensure the property has been associated
+    if (auto prop = entt::resolve<T>().prop(property_name_key); prop)
     {
-        meta_components_infos.insert({type_info.hash(),
-                                      {.name = prop.value().template cast<std::string>(),
-                                       .type_info = type_info}});
+        meta_components_infos.insert(
+          {type_info.hash(),
+           {.name = prop.value().template cast<std::string>(), .type_info = type_info}});
     }
+}
+}    // namespace impl
+
+template<typename T>
+inline void register_component(std::string &&reflected_name)
+{
+    using namespace entt::literals;
+    using namespace impl;
+
+    impl::fn<T>(std::move(reflected_name));
 }
 
 const Meta_infos &get_reflected_components_infos();
